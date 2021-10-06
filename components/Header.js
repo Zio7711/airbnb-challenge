00ccb35b7510d9e32b3687ccf9f9ec5813fa32f9
zio7711 <zi.ao.tan@outlook.com>
2021-10-06 17:49:43 +0800

    add search field in header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
+
 const Header = () => {
   return (
-    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10'>
       <div className='relative flex items-center h-10 cursor-pointer my-auto'>
         <Image
           src='https://links.papareact.com/qd3'
@@ -11,7 +13,10 @@ const Header = () => {
         />
       </div>
 
-      <div></div>
+      <div>
+        <input type='text' placeholder='Start your search' />
+        <SearchIcon className='h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer' />
+      </div>
 
       <div></div>
     </header>
