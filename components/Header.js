fdcdbf0250db2dad404648cd72660d3492992871
zio7711 <zi.ao.tan@outlook.com>
2021-10-06 17:36:28 +0800

    create header component

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+const Header = () => {
+  return (
+    <header>
+      <h1>I am the header</h1>
+      <div>
+        <Image src='https://links.papareact.com/qd3' layout='fill' />
+      </div>
+
+      <div></div>
+
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
