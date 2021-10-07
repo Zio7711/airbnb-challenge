f6a512ba82cad31a4fd5c8a611369ad149bc0675
zio7711 <zi.ao.tan@outlook.com>
2021-10-07 09:40:57 +0800

    create medium card component

---
+++ components/MediumCard.js
@@ -0,0 +1,5 @@
+const MediumCard = () => {
+  return <div></div>;
+};
+
+export default MediumCard;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
-const Home = ({ exploreData }) => {
+const Home = ({ exploreData, cardsData }) => {
   return (
     <div className=''>
       <Head>
@@ -17,13 +18,24 @@ const Home = ({ exploreData }) => {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
-          {exploreData.map((item) => (
-            <SmallCard
-              key={item.img}
-              img={item.img}
-              distance={item.distance}
-              location={item.location}
-            />
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData.map((item) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+
+          {cardsData.map((items) => (
+            <MediumCard />
           ))}
         </section>
       </main>
@@ -36,9 +48,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const cardsData = await fetch('https://links.papareact.com/zp1').then((res) =>
+    res.json()
+  );
+
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
