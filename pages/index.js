1576aff30190c84c6117a5abf4a86e2a8fa5cc81
zio7711 <zi.ao.tan@outlook.com>
2021-10-07 09:52:19 +0800

    style the medium card componnet

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,14 @@ const Home = ({ exploreData, cardsData }) => {
         <section>
           <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
 
-          {cardsData.map((items) => (
-            <MediumCard />
-          ))}
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData.map((item) => (
+              <MediumCard key={item.img} img={item.img} title={item.title} />
+            ))}
+          </div>
         </section>
+
+        <section></section>
       </main>
     </div>
   );
