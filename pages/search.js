33734cb030882ae2ca1728063c8efadc0a4b2ff4
zio7711 <zi.ao.tan@outlook.com>
2021-10-07 16:00:54 +0800

    style the buttons

--- pages/search.js
+++ pages/search.js
@@ -7,14 +7,16 @@ const Search = () => {
       <Header />
 
       <main className='flex'>
-        <section>
+        <section className='flex-grow pt-14 px-6'>
           <p className='text-xs'>300+ stays for </p>
           <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
 
-          <div className='hidden md:inline-flex'>
-            <button className='px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out'>
-              Cancellation Flexibility
-            </button>
+          <div className='hidden md:inline-flex mb-5 lg:inline-flex space-x-3 text-gray-800 whitespace-nowrap'>
+            <button className='button'>Cancellation Flexibility</button>
+            <button className='button'>Type of Places</button>
+            <button className='button'>Price</button>
+            <button className='button'>Rooms ond Beds</button>
+            <button className='button'>More filters</button>
           </div>
         </section>
       </main>
