9fe37da64dce46dea715dc24e73377215c75c114
zio7711 <zi.ao.tan@outlook.com>
2021-10-07 10:01:21 +0800

    add and style the large card component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
@@ -41,7 +42,14 @@ const Home = ({ exploreData, cardsData }) => {
           </div>
         </section>
 
-        <section></section>
+        <section>
+          <LargeCard
+            img='https://links.papareact.com/4cj'
+            title='The Greatest Outdoors'
+            description='Wishlists curated by Airbnb.'
+            buttonText='Get Inspired'
+          />
+        </section>
       </main>
     </div>
   );
