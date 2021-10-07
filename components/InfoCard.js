7b426ddcb244685f2adb248b5b4d65cffc96bc35
zio7711 <zi.ao.tan@outlook.com>
2021-10-07 16:40:48 +0800

    add img to the card component

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 const InfoCard = ({
   img,
   location,
@@ -7,7 +8,13 @@ const InfoCard = ({
   price,
   total,
 }) => {
-  return <div></div>;
+  return (
+    <div>
+      <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
+        <Image src={img} layout='fill' objectFit='cover' />
+      </div>
+    </div>
+  );
 };
 
 export default InfoCard;
