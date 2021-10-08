b20e4d588e663bae56ad5c002478602a588a1bcc
zio7711 <zi.ao.tan@outlook.com>
2021-10-08 11:24:19 +0800

    add marker and popoup on the map

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { getCenter } from 'geolib';
+import { LocationMarkerIcon } from '@heroicons/react/solid';
 
 const Map = ({ searchResults }) => {
   //transform search results to results needed
@@ -19,13 +20,50 @@ const Map = ({ searchResults }) => {
     zoom: 11,
   });
 
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   return (
     <ReactMapGL
       {...viewport}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
       mapStyle='mapbox://styles/zio7711/ckuhrc3fe9xz718s0d2xplm9b'
       mapboxApiAccessToken={process.env.mapbox_key}
-    ></ReactMapGL>
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              className='cursor-pointer text-2xl animate-bounce relative w-10'
+              aria-label='push-pin'
+              onClick={() => {
+                setSelectedLocation(result);
+              }}
+            >
+              <LocationMarkerIcon layout='fill' />
+            </p>
+          </Marker>
+          {selectedLocation.long === result.long ? (
+            <Popup
+              closeOnClick={true}
+              longitude={result.long}
+              latitude={result.lat}
+              onClose={() => {
+                setSelectedLocation({});
+              }}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
   );
 };
 
